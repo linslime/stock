96c4eb5cc1efa7abc23324ea874504821305dc8b
linslime <linslime@foxmail.com>
2024-07-25 21:12:12 +0800

    添加、修改注释，代码优化

--- get_stock_daily_data.py
+++ get_stock_daily_data.py
@@ -3,56 +3,65 @@
 '''
 
 import akshare as ak
-from multiprocessing import Process, Lock, Value
+import multiprocessing
 import os
 
-'''
-stock_code_list : 表示任务列表，其中包含各个股票的代码
-index : 表示任务列表的下标，是一个共享变量
-lock : 锁，用来同步控制多个进程访问任务列表下标
-'''
-def get_stock_daily(stock_code_list, index, lock):
 
+def get_stock_daily(stock_code):
+    """
+    函数功能 :根据股票代码下载股票每日数据
+
+    入参 :
+    stock_code : 股票代码
+    """
+
+    # 下载数据从上市到2024年06月30日
     end_date = '20240630'
-    while True:
-        lock.acquire()
-        if index.value == len(stock_code_list):
-            lock.release()
-            break
-        stock_code = stock_code_list[index.value]
-        index.value += 1
-        lock.release()
-        
-        start_date = ak.stock_individual_info_em(symbol=stock_code)['value'][7]
-        stock_daily_data = ak.stock_zh_a_hist(symbol=stock_code, period="daily", start_date=start_date, end_date=end_date)
+    # 获取上市时间
+    start_date = ak.stock_individual_info_em(symbol=stock_code)['value'][7]
+    # 获取数据
+    stock_daily_data = ak.stock_zh_a_hist(symbol=stock_code, period="daily", start_date=start_date, end_date=end_date)
+    # 保存数据到本地，GBK为windows系统的默认编码方式
+    stock_daily_data.to_csv('./data/stock_daily_data/' + stock_code + '.csv', encoding='GBK')
 
-        #GBK为windows系统的默认编码方式
-        stock_daily_data.to_csv('./data/stock_daily_data/' + stock_code + '.csv', encoding='GBK')
+
+def run(stock_code_list):
+    """
+    函数方法 : 多进程下载股票数据
+
+    入参 ：
+    stock_code_list : 共享队列，内容为股票代码
+    """
+    while not stock_code_list.empty():
+        stock_code = stock_code_list.get()
+        get_stock_daily(stock_code)
 
 
 if __name__ == "__main__":
-    #获取所有沪深京A股股票代码
+    # 获取所有沪深京A股股票代码
     stock_zh_a_spot_em_df = ak.stock_zh_a_spot_em()
     stock_code_list = stock_zh_a_spot_em_df['代码'].tolist()
 
-    #获取已完成的股票代码
+    # 获取已完成的股票代码
     completed_task_list = os.listdir('./data/stock_daily_data/')
     completed_task_list = [i.split('.')[0] for i in completed_task_list]
 
-    #获取未完成的所有股票代码
+    # 获取未完成的所有股票代码
     stock_code_list = list(set(stock_code_list) - set(completed_task_list))
 
-    #股票代码索引，是一个共享变量
-    index = Value('i', 0)
-    #控制同步互斥地访问股票代码索引
-    lock = Lock()
-    results = []
+    # 创建共享队列
+    queue = multiprocessing.Queue()
+    for stock_code in stock_code_list:
+        queue.put(stock_code)
 
-    for i in range(20):
-        result = Process(target=get_stock_daily, args=(stock_code_list, index, lock, ))
-        result.start()
-        result.join()
-        results.append(result)
+    # 多进程
+    processes = []
+    for i in range(50):
+        process = multiprocessing.Process(target=run, args=(queue, ))
+        process.start()
+        processes.append(process)
+    for process in processes:
+        process.join()
 
 
 
