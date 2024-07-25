e9c204cc94923ae2deebff5627df14077a6be8c1
linslime <linslime@foxmail.com>
2024-07-25 21:50:27 +0800

    代码格式修改

--- analyse.py
+++ analyse.py
@@ -1,18 +1,19 @@
-'''
+"""
 针对某一随机测略的多次回测结果，求得最终收益的概率密度分布
-'''
+"""
 
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-'''
-函数功能 : 加载样本数据，返回一个有序列表
 
-入参介绍 :
-sample_data_path:样本数据地址
-'''
 def load_sample_data(sample_data_path):
+    """
+    函数功能 : 加载样本数据，返回一个有序列表
+
+    入参 :
+    sample_data_path:样本数据地址
+    """
     data = pd.read_csv(filepath_or_buffer=sample_data_path, header=None)
     data = data.values
     data = list(map(list, zip(*data)))[0]
@@ -20,19 +21,19 @@ def load_sample_data(sample_data_path):
     return data
 
 
-'''
-函数功能 : 分析样本数据，得到最终受益的概率密度函数
-
-入参介绍 :
-data : 样本数据
-start : 概率分布统计起始位置
-step : 样本数据统计间隔
-
-出参介绍 :
-x : 利润
-y : 概率密度
-'''
 def analyse(data, start, step):
+    """
+    函数功能 : 分析样本数据，得到最终受益的概率密度函数
+
+    入参介绍 :
+    data : 样本数据
+    start : 概率分布统计起始位置
+    step : 样本数据统计间隔
+
+    出参介绍 :
+    x : 利润
+    y : 概率密度
+    """
     temp = []
     x = []
     y = []
@@ -55,9 +56,8 @@ def analyse(data, start, step):
     return x, y
 
 
-
-
 if __name__ == "__main__":
+
     start = 0
     step = 0.1
     file_path = "./data/sample1.csv"
