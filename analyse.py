887b13cfb7e450ef336d3f5621a32075a57a19dd
linslime <linslime@foxmail.com>
2024-07-26 18:07:57 +0800

    多进程完成任务，计算各个样本的方差和平均值

--- analyse.py
+++ analyse.py
@@ -1,10 +1,26 @@
 """
 针对某一随机测略的多次回测结果，求得最终收益的概率密度分布
 """
-
+import multiprocessing
+import os
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+import re
+
+
+def get_sample_data_path(sample_data_path):
+    """
+    函数功能 : 得到所有待处理数据的目录
+
+    入参 :
+    sample_data_path : 待处理数据文件目录
+
+    出参 :
+    stock_path_list : 所有待处理数据的目录
+    """
+    stock_code_list = os.listdir(sample_data_path)
+    sample_data_path_list = [sample_data_path + stock_code for stock_code in stock_code_list]
+    return sample_data_path_list
 
 
 def load_sample_data(sample_data_path):
@@ -14,10 +30,7 @@ def load_sample_data(sample_data_path):
     入参 :
     sample_data_path:样本数据地址
     """
-    data = pd.read_csv(filepath_or_buffer=sample_data_path, header=None)
-    data = data.values
-    data = list(map(list, zip(*data)))[0]
-    data.sort()
+    data = pd.read_csv(filepath_or_buffer=sample_data_path, index_col=0, encoding='GBK')
     return data
 
 
@@ -39,6 +52,7 @@ def analyse(data, start, step):
     y = []
     i = 1
     j = 0
+    data = sorted(data['value'].tolist())
     while j < len(data):
         if start + i * step >= data[j]:
             temp.append(data[j])
@@ -56,17 +70,88 @@ def analyse(data, start, step):
     return x, y
 
 
+def complete_one_task(sample_data_path):
+    """
+    函数功能 ： 完成一个样本数据的计算结果
+
+    入参 :
+    sample_data_path : 样本数据的目录
+
+    出参 :
+    stock_code : 股票代码
+    mean : 多测回测结果的平均值
+    std : 多次回测结果的标准差
+    """
+    sample_data = load_sample_data(sample_data_path)
+    mean = sample_data['value'].values.mean()
+    std = sample_data['value'].values.std()
+    stock_code = re.split(pattern='[\./]', string=sample_data_path)[-2]
+    return stock_code, mean, std
+
+
+def run(sample_data_path_queue, result_queue, lock):
+    """
+    函数功能 ： 多进程处理样本数据
+    :param sample_data_path_queue: 样本数据目录队列
+    :param result_queue: 结果队列，数据处理完后放入
+    :param lock: 锁，用于互斥访问样本数据目录队列
+    :return: 无
+    """
+    while True:
+        lock.acquire()
+        if not sample_data_path_queue.empty():
+            sample_data_path = sample_data_path_queue.get()
+            lock.release()
+            result = complete_one_task(sample_data_path)
+            result_queue.put(result)
+        else:
+            lock.release()
+            break
+
+
 if __name__ == "__main__":
+    # 样本数据目录
+    sample_data_path = "./data/random_buy_and_sell/"
+    # 获取所有样本数据文件目录
+    sample_data_path_list = get_sample_data_path(sample_data_path)
 
-    start = 0
-    step = 0.1
-    file_path = "./data/sample1.csv"
-    data = load_sample_data(file_path)
-    x, y = analyse(data, start, step)
+    # 管理共享资源
+    manager = multiprocessing.Manager()
 
-    plt.rcParams['font.sans-serif'] = ['SimHei']
-    plt.rcParams['axes.unicode_minus'] = False
-    plt.plot(x[:int(len(x)/18)], y[:int(len(y)/18)], color="red" ,linewidth=1.0, linestyle="-")  # 将散点连在一起
-    plt.xlabel('利润')
-    plt.ylabel('密度')
-    plt.show()
+    # 创建样本数据目录共享队列
+    sample_data_path_queue = manager.Queue()
+    for sample_data_path in sample_data_path_list:
+        sample_data_path_queue.put(sample_data_path)
+
+    # 创建处理结果共享队列
+    result_queue = manager.Queue()
+    # 锁, 用于同步样本数据共享队列
+    lock = manager.Lock()
+
+    # 多进程完成任务
+    processes = []
+    for i in range(8):
+        process = multiprocessing.Process(target=run, args=(sample_data_path_queue, result_queue, lock))
+        process.start()
+        processes.append(process)
+    for process in processes:
+        process.join()
+
+    # 股票代码列表
+    stock_code_list = []
+    # 平均值
+    mean_list = []
+    # 标准差
+    std_list = []
+
+    # 将队列中的数据放置到列表中
+    while result_queue.qsize() > 0:
+        result = result_queue.get()
+        stock_code_list.append(result[0])
+        mean_list.append(result[1])
+        std_list.append(result[2])
+    # 构建pandas
+    results = pd.DataFrame(data={'stock_code': stock_code_list, 'mean': mean_list, 'std': std_list})
+    # 保存数据
+    results.to_csv("./data/random_buy_and_sell_result.csv")
+    print(results)
